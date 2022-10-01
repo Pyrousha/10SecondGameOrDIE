05f10918ae6545a9bc2b2e26a1baaa1a08bf0f01
travisbird0 <47787636+Pyrousha@users.noreply.github.com>
2022-09-30 21:47:22 -0500

    Room transitions

---
+++ Assets/Scripts/PlayerRoomTrigger.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRoomTrigger : MonoBehaviour
+{
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float cameraSpeed;
+
+    private Collider2D currRoom;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision == currRoom)
+            return;
+
+        currRoom = collision;
+        StartCoroutine(CameraLerp(collision.transform.position));
+    }
+
+    private IEnumerator CameraLerp(Vector3 targPos)
+    {
+        targPos.z = cameraTransform.position.z;
+
+        float dist;
+        Vector3 toMove;
+
+        while(true)
+        {
+            dist = Vector3.Distance(cameraTransform.position, targPos);
+
+            toMove = (targPos - cameraTransform.position) * cameraSpeed * Time.deltaTime;
+
+            if ((toMove.magnitude >= dist) || (dist <= 0.01f)) //close enough
+            {
+                cameraTransform.position = targPos;
+                break;
+            }
+
+            //Still got a ways to go
+            cameraTransform.position += toMove;
+
+            yield return null;
+        }
+
+        yield return null;
+    }
+}

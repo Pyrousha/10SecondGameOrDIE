9d69eae4115958fb6d0fabe7cae84faf5bd57616
travisbird0 <47787636+Pyrousha@users.noreply.github.com>
2022-10-02 17:13:26 -0500

    Grass get snipped oh yeah

--- Assets/DeathController.cs
+++ Assets/DeathController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class DeathController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class DeathController : Singleton<DeathController>
+{ 
+    [SerializeField] private GrabbableID[] grabbables;
+
+    private void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
+        ReloadScene();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDeath()
     {
-        
+        foreach (GrabbableID grab in grabbables)
+        {
+            grab.OnDeath();
+        }
+
+        ReloadScene();
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
     }
 }
